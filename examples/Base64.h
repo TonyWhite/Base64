3ce50bbb25b280a1fd4b8a937f29216055a31e82
TonyWhite <tonywhite1985@libero.it>
2022-08-09 16:51:16 +0200

    Update Base64.h

--- examples/Base64.h
+++ examples/Base64.h
@@ -6,24 +6,30 @@
 
 class Base64 {
   public:
-  static const std::string CHARSET; // Base64 charset
-  
-  static void decode(const std::string*, std::string*);       // Decode string from Base64
-  static void decode(const std::string*, std::vector<char>*); // Decode vector from Base64
-  
-  static void encode(const std::string*, std::string*);       // Encode string to Base64
-  static void encode(const std::vector<char>*, std::string*); // Encode vector to Base64
-  
-  
+  static const std::string STD_CHARSET; // Base64 standard charset
+  static const std::string URL_CHARSET; // Base64 URL safe charset
+
+  static bool decode(const std::string*, std::string*, const std::string *charset = &Base64::STD_CHARSET);       // Decode string from Base64
+  static bool decode(const std::string*, std::vector<char>*, const std::string *charset = &Base64::STD_CHARSET); // Decode vector from Base64
+
+  static void encode(const std::string*, std::string*, const std::string *charset = &Base64::STD_CHARSET, const bool padding = true);       // Encode string to Base64
+  static void encode(const std::vector<char>*, std::string*, const std::string *charset = &Base64::STD_CHARSET, const bool padding = true); // Encode vector to Base64
+
+  static bool is_standard(const std::string*); // Check if the encoded string respects the standard charset.
+  static bool is_url_safe(const std::string*); // Check if the encoded string respects the URL safe charset.
+
   private:
   // Base64 core
-  static void decode(const char*, const long unsigned int, char*);        // Decode from Base64
-  static void encode(const char*, const long unsigned int, std::string*); // Encode to Base64
-  
-  static inline bool isBase64(char); // Check for a valid Base64 char
-  
-  static inline long unsigned int predict_enc_len(const long unsigned int); // Predict length of encoded data
-  static inline long unsigned int predict_dec_len(const std::string*);      // Predict length of decoded data
+  static bool decode(const char*, const unsigned long, char*, const std::string*);                            // Decode from Base64
+  static void encode(const char*, const unsigned long, std::string*, const std::string*, const bool padding); // Encode to Base64
+
+  // Respect the charset?
+  static bool is_base64(const std::string*, const std::string*); // Check if the encoded string respect the given charset.
+  static inline bool is_valid_char(char, const std::string*);    // Check if the char respect the given charset.
+
+  // Predict the length of result
+  static inline unsigned long predict_enc_len(const unsigned long, const bool); // Predict the length of encoded data
+  static inline unsigned long predict_dec_len(const std::string*);              // Predict the length of decoded data
 };
 
 #endif
